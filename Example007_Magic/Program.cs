25c8bb39089bc13ee291cb276dc56df392804045
Elbrus5642 <satellitprof@gmail.com>
2022-11-28 15:51:31 +0300

    Треугольник Карпинского

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 20;
+int xc = 30;
+int yc = 20;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
