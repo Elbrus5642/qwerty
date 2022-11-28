c5cb4f1cdb1934cff0d8627f634282a48ebcddcc
Elbrus5642 <satellitprof@gmail.com>
2022-11-28 14:40:54 +0300

    Random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA =  31;
-double numberB = 7;
-double div = numberA / numberB;
+﻿int numberA = new Random().Next(1,10);//Любое целое число от 1 до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int div = numberA / numberB;
 Console.WriteLine(div);
