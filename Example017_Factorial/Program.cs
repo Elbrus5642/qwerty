96f505b8ce017ad7026832b2bd5b8f9f9d0fd483
Elbrus5642 <satellitprof@gmail.com>
2022-12-15 12:33:51 +0300

    Факториал

---
+++ Example017_Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿// Вычислить факториал числа N: n!
+//int n = 6;
+double Factorial(int n)
+{
+    if (n == 1  || n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n - 1);
+    }
+}
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+//Console.WriteLine($"{n} ! = {Factorial(n)}");
